4bd2347986d3a26b3c8ff19a597e8ba3997fc61f
WillParch <lilpez2000@gmail.com>
2023-03-05 21:14:55 -0500

    Almost-Almost Commit

--- Project2_Hallway_With_Guard/Assets/Scripts/EnemyController.cs
+++ Project2_Hallway_With_Guard/Assets/Scripts/EnemyController.cs
@@ -7,11 +7,18 @@ public class EnemyController : MonoBehaviour
     public int damage;
     public PlayerHealth playerHealth;
     public GameObject roof;
-   
+    public GameObject eye1;
+    public GameObject eye2;
     public GameObject fire;
     public GameObject roomLight;
     public GameObject roomLight2;
     public float interval = 1f;
+
+    private void Start()
+    {
+        eye1.gameObject.SetActive(false);
+        eye2.gameObject.SetActive(false);
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
@@ -22,18 +29,24 @@ public class EnemyController : MonoBehaviour
         if(collision.gameObject.tag == "Barrier")
         {
             Destroy(roof);
+            eye1.gameObject.SetActive(true);
+            eye2.gameObject.SetActive(true);
             
         }
 
         if(collision.gameObject.tag == "Barrier2")
         {
             StartCoroutine(Flame());
+            eye1.gameObject.SetActive(true);
+            eye2.gameObject.SetActive(true);
         }
 
         if(collision.gameObject.tag == "Barrier3")
         {
             Destroy(roomLight);
             Destroy(roomLight2);
+            eye1.gameObject.SetActive(true);
+            eye2.gameObject.SetActive(true);
         }
     }
 
